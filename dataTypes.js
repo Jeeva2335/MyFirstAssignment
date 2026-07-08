612f60bf954369513b9e40eec61d8e72c30dd638
Jeeva2335 <42792132+Jeeva2335@users.noreply.github.com>
2026-07-08 10:09:13 +0530

    Add files via upload
    Week1 first assignment

---
+++ dataTypes.js
@@ -0,0 +1,29 @@
+let firstName = "Jeeva"
+console.log(firstName)
+console.log(typeof firstName)
+
+let companyName = "OneVerse"
+console.log(companyName)
+console.log(typeof companyName)
+
+let mobileNumber = 1234567890
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+let isAutomation = true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+let hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
+
+
+
+
+
+
+
+
+
+
